8e5ff4a0d981d4e143e8727480ed34c3c327bdc3
Marc Hendri Soquiat <>
2025-11-15 23:04:55 +0800

    Test Pipe pt.2

--- root/main.tf
+++ root/main.tf
@@ -26,7 +26,7 @@ resource "aws_lambda_function" "lb-func" {
 
   environment {
     variables = {
-      GREETING = "Greetings everyone!"
+      GREETING = "Greetings everyone, from Soki!"
     }
   }
 }
