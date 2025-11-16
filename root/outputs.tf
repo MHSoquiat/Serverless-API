500380899bef83ee0fcc90bb682264153aba4be8
Marc Hendri Soquiat <>
2025-11-16 21:56:30 +0800

    Modified Pipeline

--- root/outputs.tf
+++ root/outputs.tf
@@ -1,4 +1,4 @@
 output "api-endpoint" {
   description = "URL of the deployed HTTP API"
-  value = aws_apigatewayv2_stage.default.invoke_url
+  value       = aws_apigatewayv2_stage.default.invoke_url
 }
