92a1b59a1e03eabd24daad085af2ff5c5e6c3aaf
Marc Hendri Soquiat <>
2025-11-16 14:53:37 +0800

    Filter gibberish characters

--- statefile-locking/main.tf
+++ statefile-locking/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "statefile" {
-  bucket = "statefile-soki-lb-api-v3"
+  bucket = "statefile-soki-lb-api-v4"
 
   lifecycle {
     prevent_destroy = true
